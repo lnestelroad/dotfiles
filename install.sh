2407f3dfae07ab7e41afd7629d0de237c59e2a08
Liam Nestelroad <nestelroadliam@gmail.com>
2022-05-15 23:03:09 -0600

    fix(Tmux): Add non sude command prefix for tmux

--- install.sh
+++ install.sh
@@ -71,15 +71,15 @@ sudo -u $SUDO_USER git clone https://github.com/VundleVim/Vundle.vim.git /home/$
 sudo -u $SUDO_USER vim +visual +PluginInstall +qall
 
 # Tmux setup
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sudo -u $SUDO_USER git clone https://github.com/tmux-plugins/tpm /home/$SUDO_USER/.tmux/plugins/tpm
 # start a server but don't attach to it
-tmux start-server
+sudo -u $SUDO_USER tmux start-server
 # create a new session but don't attach to it either
-tmux new-session -d
+sudo -u $SUDO_USER tmux new-session -d
 # install the plugins
-~/.tmux/plugins/tpm/scripts/install_plugins.sh
+sudo -u $SUDO_USER /home/$SUDO_USER/.tmux/plugins/tpm/scripts/install_plugins.sh
 # killing the server is not required, I guess
-tmux kill-server
+sudo -u $SUDO_USER tmux kill-server
 
 # Soft link config files in home directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
